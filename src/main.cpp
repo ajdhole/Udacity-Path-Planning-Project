9fac9b381a7871de36d23db931a5b7a6362525ef
Anil Dhole <ajdhole@gmail.com>
2018-02-10 19:15:53 +0530

    fulfils project requirements.

--- src/main.cpp
+++ src/main.cpp
@@ -244,54 +244,84 @@ int main() {
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
+            //Provided previous path point size.
             int prev_size = previous_path_x.size();
 
+            //Preventing collition
             if (prev_size > 0)
             {
               car_s = end_path_s;
             }
 
+            //Prediction -Analysing other cars position
+
             bool too_close = false;
+            bool car_left = false;
+            bool car_right = false;
 
             //find ref_v to use
             for (int i = 0; i < sensor_fusion.size(); i++)
             {
-              //car is in my lane
+              //checking car lane position
               float d = sensor_fusion[i][6];
-              if(d < (2+4*lane+2) && d > (2+4*lane-2))
-              {
+              int car_lane = 0;
+              // is it on the same lane we are
+              if ( d > 0 && d < 4 ) {
+                car_lane = 0;
+              } else if ( d > 4 && d < 8 ) {
+                car_lane = 1;
+              } else if ( d > 8 && d < 12 ) {
+                car_lane = 2;
+              }
+              if (car_lane < 0) {
+                continue;
+}
+
+              //Find the car speed.
                 double vx = sensor_fusion[i][3];
                 double vy = sensor_fusion[i][4];
                 double check_speed = sqrt(vx*vx+vy*vy);
                 double check_car_s = sensor_fusion[i][5];
 
-                check_car_s += ((double)prev_size*0.02*check_speed); //
+                //Estimating car s position after excuting previous trajectory.
+                check_car_s += ((double)prev_size*0.02*check_speed);
+
                 //check for s value
-                if ((check_car_s > car_s) && ((check_car_s-car_s) < 30))
-                {
-                  //set ref velocity
-
-                  //ref_vel = 29.5;
-                  too_close = true;
-                  if(lane > 0)
-                  {
-                    lane = 0;
-                  }
-
+                if ( car_lane == lane ) {
+                  // Car in our lane.
+                  too_close |= check_car_s > car_s && check_car_s - car_s < 30;
+                } else if ( car_lane - lane == -1 ) {
+                  // Car left
+                  car_left |= car_s - 30 < check_car_s && car_s + 30 > check_car_s;
+                } else if ( car_lane - lane == 1 ) {
+                  // Car right
+                  car_right |= car_s - 30 < check_car_s && car_s + 30 > check_car_s;
                 }
-
               }
-
-            }
-
-            if(too_close)
-            {
-              ref_vel -= 0.224;
-            }
-            else if(ref_vel < 49.5)
-            {
-              ref_vel += 0.224;
-            }
+              //Behavioral Planning.
+              double speed_diff = 0;
+              const double MAX_SPEED = 49.5;
+              const double MAX_ACC = .224;
+              if ( too_close ) { // Car ahead
+                if ( !car_left && lane > 0 ) {
+                // if there is no car left and there is a left lane.
+                  lane--; // Change lane left.
+                } else if ( !car_right && lane != 2 ){
+                // if there is no car right and there is a right lane.
+                  lane++; // Change lane right.
+                } else {
+                  speed_diff -= MAX_ACC;
+                }
+              } else {
+                if ( lane != 1 ) { // if we are not on the center lane.
+                  if ( ( lane == 0 && !car_right ) || ( lane == 2 && !car_left ) ) {
+                    lane = 1; // Back to center.
+                  }
+                }
+                if ( ref_vel < MAX_SPEED ) {
+                  speed_diff += MAX_ACC;
+                }
+              }
 
           	vector<double> ptsx;
           	vector<double> ptsy;
@@ -384,9 +414,14 @@ int main() {
             double x_add_on = 0;
 
             // Fill up the rest of Path planner after filling it with previous points, here we will always output 50 points.
+            for( int i = 1; i < 50 - prev_size; i++ ) {
+              ref_vel += speed_diff;
+              if ( ref_vel > MAX_SPEED ) {
+                ref_vel = MAX_SPEED;
+              } else if ( ref_vel < MAX_ACC ) {
+                ref_vel = MAX_ACC;
+              }
 
-            for (int i=1; i <= 50-previous_path_x.size(); i++)
-            {
               double N = (target_dist/(0.02*ref_vel/2.24));
               double x_point = x_add_on+(target_x)/N;
               double y_point = s(x_point);
